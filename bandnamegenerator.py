0e2fef86a19c65f62b0e134bc199079a655e5922
Lisa Mays <64438827+Lisa-Mays@users.noreply.github.com>
2021-08-20 20:14:47 -0400

    Add files via upload
    Band name generator for 100 days of code.

---
+++ bandnamegenerator.py
@@ -0,0 +1,13 @@
+# Create a greeting for your program.
+print("Welcome to the Band Name Generator!")
+
+# Ask the user for the city that they grew up in.
+city_name = input("What city did you grow up in?\n")
+
+# Ask the user for the name of a pet.
+pet_name = input("What is the name of your favorite pet?\n")
+
+# Combine the name of their city and pet and show them their band name.
+print("Your band name is: " + city_name + " " + pet_name)
+
+# Make sure the input cursor shows on a new line
